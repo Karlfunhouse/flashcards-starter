e5c9214bda04cc7e46d63ad4752e0b31af42cb09
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-03-19 18:48:33 -0600

    :crystal_ball: finished start method, removed round from printMessage parameters

--- src/Game.js
+++ src/Game.js
@@ -9,22 +9,24 @@ const Round = require ('../src/Round');
 class Game {
   constructor() {
     this.currentRound = null;
-  }
+  };
 
   start() {
     const deck = new Deck(prototypeQuestions);
     const round = new Round(deck);
-    
-  }
+    this.currentRound = round;
+    this.printMessage(deck);
+    this.printQuestion(round);
+  };
 
   printMessage(deck) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
-  }
+};
 
   printQuestion(round) {
       util.main(round);
-  }
-}
+  };
+};
 
 module.exports = Game;
